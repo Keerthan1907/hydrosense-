ca1cfe32a670e4a6201945449f6995bb8bc625a3
Keerthan1907 <124329486+Keerthan1907@users.noreply.github.com>
2026-01-24 13:05:39 +0530

    Fix increment of index 'i' in merge function
    Fixed bug by incrementing index 'i' in merge function.

--- merge.py
+++ merge.py
@@ -16,17 +16,14 @@ def merge(left, right):
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             result.append(left[i])
-            i += 1
+            # BUG: forgot to increment i
         else:
             result.append(right[j])
             j += 1
-
-    # Add remaining elements
-    result.extend(left[i:])
-    result.extend(right[j:])
 
     return result
 
 
 arr = [38, 27, 43, 3, 9, 82, 10]
 print("Sorted array:", merge_sort(arr))
+
