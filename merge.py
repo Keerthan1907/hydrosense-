b1712c1653798e04d12bde379217c26a454726bb
Keerthan1907 <124329486+Keerthan1907@users.noreply.github.com>
2026-02-10 12:04:29 +0530

    Remove merge_sort function from merge.py
    Removed the merge_sort function definition.

--- merge.py
+++ merge.py
@@ -1,12 +1,4 @@
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
 
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-
-    return merge(left, right)
 
 
 def merge(left, right):
